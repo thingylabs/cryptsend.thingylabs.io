04d2ce7b51d6e22493bd16f4928143f9e3805a9b
idrathernotgetmails <idrathernotgetmails@gmail.com>
2025-01-29 16:06:41 +0000

    Cache bust for the CSS file

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,5 +1,6 @@
 // routes/_app.tsx
 import { AppProps } from '$fresh/server.ts'
+import { asset } from '$fresh/runtime.ts'
 
 export default function App({ Component }: AppProps) {
   return (
@@ -7,7 +8,7 @@ export default function App({ Component }: AppProps) {
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <link rel='stylesheet' href='/styles.css' />
+        <link rel='stylesheet' href={asset('/styles.css')} />
       </head>
       <body className='bg-white text-black dark:bg-gray-900 dark:text-white transition-colors duration-300'>
         <Component />
