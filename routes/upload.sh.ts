4f3ed07b6e71f8b237cea1a4d927be45d26a5fbc
Jan Guth <Jan.Guth@gmail.com>
2025-02-03 13:34:20 +0100

    beautify bash
    reformat bash script

--- routes/upload.sh.ts
+++ routes/upload.sh.ts
@@ -1,23 +1,60 @@
 // routes/upload.sh.ts
 import { Handlers } from '$fresh/server.ts'
 
-const script = `#!/bin/bash
-if [ "$#" -ne 1 ]; then
+// TODO: move this into a bash file within the root directory
+const script = `#!/usr/bin/env bash
+set -euo pipefail
+
+usage() {
   echo "Usage: $0 <filename>"
   exit 1
-fi
+}
 
-FILENAME=$(basename "$1")
-KEY=$(openssl rand -hex 32)
-IV=$(openssl rand -hex 16)
-ENCODED_FILENAME=$(echo -n "$FILENAME" | xxd -plain | tr -d '\n' | sed 's/\\(.\\{2\\}\\)/%\\1/g')
+prerequisites() {
+  command -v openssl @>/dev/null || {
+    echo "openssl is required!"
+    exit 2
+  }
+}
 
-# Use key as hash for storage
-openssl enc -aes-256-cbc -in "$1" -K "$KEY" -iv "$IV" | \\
-curl -s -X PUT "https://cryptsend.thingylabs.io/\${KEY}.enc" \\
-  -H "Content-Type: application/octet-stream" \\
-  --data-binary @- > /dev/null && \\
-echo "https://cryptsend.thingylabs.io/d/#\${KEY}\${IV}\${ENCODED_FILENAME}"
+file() {
+  # one argument required
+  [ "\${#}" -eq 1 ] || usage()
+  
+  local file="${1}"
+  
+  # needs to be a file
+  [ -f "\${file}" ] || usage()
+
+  # todo: check if it is a directory
+  # if so zip after confirmation
+
+  echo "\${file}"
+}
+
+upload() {
+  local file="\${1}"
+  local filename="$(basename "\${file}")"
+  local key="$(openssl rand -hex 32)"
+  local iv="$(openssl rand -hex 16)"
+  local encoded_filename
+  
+  encoded_filename="$(
+    echo -n "\${filename}" | xxd -plain | tr -d '\n' | sed 's/\\(.\\{2\\}\\)/%\\1/g')
+  )"
+  
+  # Use key as hash for storage
+  openssl \\
+    enc -aes-256-cbc -in "\${file}" -K "\${key}" -iv "\${iv}" |
+  curl -s -X PUT "https://cryptsend.thingylabs.io/\${key}.enc" \\
+    -H "Content-Type: application/octet-stream" \\
+    --data-binary @- > /dev/null
+
+  echo "https://cryptsend.thingylabs.io/d/#\${key}\${iv}\${encoded_filename}"
+}
+
+prerequisites
+upload "$(file)"
 `
 
 export const handler: Handlers = {
