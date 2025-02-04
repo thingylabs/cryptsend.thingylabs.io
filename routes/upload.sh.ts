8082f5ae1f6f1dc1239bc7467a1d55d98490cb5c
Jan Guth <Jan.Guth@gmail.com>
2025-02-04 12:48:26 +0100

    move bash to upload.sh and add directory support

--- routes/upload.sh.ts
+++ routes/upload.sh.ts
@@ -1,61 +1,8 @@
 // routes/upload.sh.ts
 import { Handlers } from '$fresh/server.ts'
+import { readFileSync } from 'fs'
 
-// TODO: move this into a bash file within the root directory
-const script = `#!/usr/bin/env bash
-set -euo pipefail
-
-usage() {
-  echo "Usage: $0 <filename>"
-  exit 1
-}
-
-prerequisites() {
-  # one argument required
-  [ "\${#}" -eq 1 ] || usage()
-  
-  command -v openssl @>/dev/null || {
-    echo "openssl is required!"
-    exit 2
-  }
-}
-
-file() {
-  local file="\${1}"
-  
-  # needs to be a file
-  [ -f "\${file}" ] || usage()
-
-  # todo: check if it is a directory
-  # if so zip after confirmation
-
-  echo "\${file}"
-}
-
-upload() {
-  local file="\${1}"
-  local filename encoded_filename key iv
-  
-  filename="$(basename "\${file}")"
-  key="$(openssl rand -hex 32)"
-  iv="$(openssl rand -hex 16)"
-  encoded_filename="$(
-    echo -n "\${filename}" | xxd -plain | tr -d '\n' | sed 's/\\(.\\{2\\}\\)/%\\1/g')
-  )"
-  
-  # Use key as hash for storage
-  openssl \\
-    enc -aes-256-cbc -in "\${file}" -K "\${key}" -iv "\${iv}" |
-  curl -s -X PUT "https://cryptsend.thingylabs.io/\${key}.enc" \\
-    -H "Content-Type: application/octet-stream" \\
-    --data-binary @- > /dev/null
-
-  echo "https://cryptsend.thingylabs.io/d/#\${key}\${iv}\${encoded_filename}"
-}
-
-prerequisites "${@}"
-upload "$(file "\${1}")"
-`
+const script = readFileSync('../upload.sh', 'utf-8')
 
 export const handler: Handlers = {
   GET() {
